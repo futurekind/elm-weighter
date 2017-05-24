8ed0fda883e89f797af020c4c85f75b75e49b1f7
Fabian Trinler <fabian.trinler@blateral.com>
2017-05-24 17:58:28 +0200

    Adding basic page transitions

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,24 +1,39 @@
 module Main exposing (..)
 
 import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
 
 type alias Model =
-    {}
+    { pageIndex : Int }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { pageIndex = 0
+      }
+    , Cmd.none
+    )
 
 
 type Msg
-    = Noop
+    = IncrementPageIndex
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        IncrementPageIndex ->
+            ( { model | pageIndex = getPageIndex <| model.pageIndex + 1 }, Cmd.none )
+
+
+getPageIndex : Int -> Int
+getPageIndex nextIndex =
+    if Debug.log "next" nextIndex == 3 then
+        0
+    else
+        nextIndex
 
 
 subscriptions : Model -> Sub Msg
@@ -28,7 +43,49 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    text "Hey App"
+    div
+        [ class "pages"
+        , onClick IncrementPageIndex
+        ]
+        [ div
+            [ class "page"
+            , style
+                [ ( "background", "red" )
+                , ( "transform", getTranslateValue 0 model.pageIndex )
+                ]
+            ]
+            [ text "Rot" ]
+        , div
+            [ class "page"
+            , style
+                [ ( "background", "green" )
+                , ( "transform", getTranslateValue 1 model.pageIndex )
+                ]
+            ]
+            [ text "green" ]
+        , div
+            [ class "page"
+            , style
+                [ ( "background", "blue" )
+                , ( "transform", getTranslateValue 2 model.pageIndex )
+                ]
+            ]
+            [ text "blue" ]
+        ]
+
+
+getTranslateValue : Int -> Int -> String
+getTranslateValue index pageIndex =
+    let
+        value =
+            if index == pageIndex then
+                0
+            else if index < pageIndex then
+                -100
+            else
+                100
+    in
+        "translateX(" ++ (toString value) ++ "%)"
 
 
 main : Program Never Model Msg
