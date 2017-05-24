f4866e21325f279f0d64df4c1dfaeb1b88cf68dd
Fabian Trinler <fabian.trinler@blateral.com>
2017-05-24 16:50:26 +0200

    Adding program

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -3,5 +3,39 @@ module Main exposing (..)
 import Html exposing (..)
 
 
+type alias Model =
+    {}
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( {}, Cmd.none )
+
+
+type Msg
+    = Noop
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+view : Model -> Html Msg
+view model =
+    text "Hey App"
+
+
+main : Program Never Model Msg
 main =
-    text "Hey"
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
