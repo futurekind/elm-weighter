1154b89b4fe12d3f42d7123082a5cbd93488b1b7
Fabian Trinler <fabian.trinler@blateral.com>
2017-06-29 09:53:01 +0200

    Changing page index

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -29,7 +29,7 @@ type alias ServerData =
 
 init : ( Model, Cmd Msg )
 init =
-    ( { pageIndex = 1
+    ( { pageIndex = 0
       , touchStartX = 0.0
       , pages =
             [ { class = "page--enter" }
